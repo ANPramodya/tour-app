6ea7bf58fd503e5073fa02f10ecdce6285399f56
ANPramodya <86116471+ANPramodya@users.noreply.github.com>
2023-07-31 01:12:34 +0530

    common css

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./navbar.css";
+
+const Navbar = () => {
+  return (
+    <section className="navBarSection">
+      <div className="header">
+        <div className="logoDiv">
+          <a href="#" className="logo"></a>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Navbar;
---
+++ src/components/popular/Popular.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./popular.css";
+
+const Popular = () => {
+  return <div>Popular</div>;
+};
+
+export default Popular;
