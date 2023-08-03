4367b7ee2bce54223853619ecb9c3915685e5099
ANPramodya <86116471+ANPramodya@users.noreply.github.com>
2023-08-03 23:23:57 +0530

    Home Aos

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,34 +1,57 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./home.css";
 
+import Aos from "aos";
+import "aos/dist/aos.css";
+
 const Home = () => {
+  useEffect(() => {
+    Aos.init({ duration: 2000 });
+  }, []);
+
   return (
     <section className="home">
       <div className="secContainer container">
         <div className="homeText">
-          <h1 className="title">Plan Your Trip With Travel Dot</h1>
-          <p className="subTitle">
+          <h1 data-aos="fade-up" className="title">
+            Plan Your Trip With Travel Dot
+          </h1>
+          <p data-aos="fade-up" data-aos-duration="2500" className="subTitle">
             Travel to your city with respect to your environment!
           </p>
-          <button className="btn">
+          <button data-aos="fade-up" data-aos-duration="3000" className="btn">
             <a href="#">Explore Now</a>
           </button>
         </div>
 
         <div className="homeCard grid">
-          <div className="locationDiv">
+          <div
+            data-aos="fade-right"
+            data-aos-duration="2000"
+            className="locationDiv"
+          >
             <label htmlFor="location">Location</label>
             <input type="text" placeholder="Dream Destination" />
           </div>
-          <div className="distDiv">
+          <div
+            data-aos="fade-right"
+            data-aos-duration="2500"
+            className="distDiv"
+          >
             <label htmlFor="distance">Location</label>
             <input type="text" placeholder="11/Meters" />
           </div>
-          <div className="priceDiv">
+          <div
+            data-aos="fade-right"
+            data-aos-duration="3000"
+            className="priceDiv"
+          >
             <label htmlFor="price">Location</label>
             <input type="text" placeholder="$140-$500" />
           </div>
-          <button className="btn">Search</button>
+          <button data-aos="fade-left" data-aos-duration="2000" className="btn">
+            Search
+          </button>
         </div>
       </div>
     </section>
